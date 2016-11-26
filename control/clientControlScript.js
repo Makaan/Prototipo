da5d460e8e1634dec8c327748e7873a1ac0cf2bd
Makaan <nestor.figliuolo@gmail.com>
2016-11-26 20:47:24 -0300

    Pruebas y mas pruebas

--- control/clientControlScript.js
+++ control/clientControlScript.js
@@ -4,13 +4,63 @@ var socket = io('/control');
 
 $( document ).ready(function() {
   
-  socket.connect('http://192.168.0.100:3000');
-  console.log('conectado '+(socket.connected));
-  $('#boton').click(function() {
-    var x=$('#x').val();
-    var y=$('#y').val();
-    $('#status').append('boton apretado, cord: '+x+' '+y+'<br>');
-    socket.emit('boton',{'x':x,'y':y});
+  $('#conectar').click(function conectar() {
+    //Me conecto al server
+    socket.connect('http://10.3.184.216:3000');
+    //Le envio al servidor el nombre del jugador.
+    console.log($('#nombre').val());
+    socket.emit('nombre',$('#nombre').val());
+  });
+  
+  $('#botonArriba').click(function botonArriba() {
+    socket.emit('botonArriba');
+  });
+  
+  $('#botonDerecha').mousedown(function botonArriba() {
+    socket.emit('mousedown');
+  });
+  
+   $('#botonDerecha').mouseup(function botonArriba() {
+    socket.emit('mouseup');
+  });
+  
+  $('#botonAbajo').click(function botonArriba() {
+    socket.emit('botonAbajo');
+  });
+  
+  $('#botonIzquierda').click(function botonArriba() {
+    socket.emit('botonIzquierda');
+  });
+  //Listener para tap de boton derecho
+  var botonDerecha = document.getElementById('botonDerecha');
+  var tapDerecha = new Hammer(botonDerecha);
+  // listen to events...
+  tapDerecha.on("press", function() {
+      console.log('press R');
+      socket.emit('pressDerecha');
+      
+  });
+  tapDerecha.on('pressup', function() {
+    console.log('pressup R');
+    socket.emit('pressupDerecha');
+  });
+  
+  //Listener para tap de boton izquierdo
+  var tapIzquierda = new Hammer(document.getElementById('botonIzquierda'));
+  // listen to events...
+  tapIzquierda.on("press", function() {
+      console.log('press L');
+      socket.emit('pressIzquierda');
+      
+  });
+  tapIzquierda.on('pressup', function() {
+    console.log('pressup L');
+    socket.emit('pressupIzquierda');
+  });
+  
+  var tapArriba=new Hammer(document.getElementById('botonArriba'));
+  tapArriba.on('tap',function() {
+    socket.emit('botonArriba');
   });
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app= express();
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -24,7 +24,10 @@ var controlNsp=io.of('/control');
 
 visorNsp.on('connection', function(){
   console.log("visor conectado");
-
+  //Cuando se conecta un visor dibuja todo
+  for(var i=0;i<controles.length;i++) {
+    socket.emit('dibujar',controles[i]);
+  }
 });
 
 //Controles
@@ -33,15 +36,58 @@ var controles=[];
 controlNsp.on('connection',function(socket) {
   console.log('control conectado');
   
-  //Cuando se presiona el boton en un control
-  socket.on('boton', function emitirCoordenadas(coordenadas){
-    console.log('cordenadas: '+coordenadas.x+' '+coordenadas.y);
-    visorNsp.emit('dibujar',coordenadas);
-    console.log('emitido dibujar');
+  //Cuando ser recibe el nombre
+  socket.on('nombre',function setNombre(nombreControl) {
+    var encontre=false;
+    for(var i=0;i<controles.length;i++) {
+      if(controles[i].nombre===nombreControl) {
+         encontre=true;
+         break;
+      }
+    }
+    //Si el control no existe creo uno nuevo
+    if(!encontre) {
+      var obj={
+        'nombre':nombreControl,
+        'x':500,
+        'y':300
+      };
+      controles.push(obj);
+      //Lo dibujo en todos los visores
+      visorNsp.emit('dibujar',obj);
+    }
   });
   
+  socket.on('botonArriba', function botonArriba() {
+    visorNsp.emit('botonArriba');
+  });
+  
+  socket.on('pressDerecha', function() {
+    console.log('press R');
+    visorNsp.emit('botonDerechaPress');
+  });
+  
+  socket.on('pressupDerecha', function() {
+    console.log('pressup R');
+    visorNsp.emit('botonDerechaPressup');
+  });
+  
+  socket.on('pressIzquierda', function() {
+    console.log('press L');
+    visorNsp.emit('botonIzquierdaPress');
+  });
+  
+  socket.on('pressupIzquierda', function() {
+    console.log('pressup L');
+    visorNsp.emit('botonIzquierdaPressup');
+  });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+});
+
+app.use(express.static(__dirname + '/phaser'));
+app.get('/phaser',function(req,res) {
+  res.sendFile(__dirname+'/phaser/index.html');
 });
